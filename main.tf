bd684ac8bfbb227854e83e473825e6dc22b8ec39
Dinesh <satyadineshk328@gmail.com>
2023-04-03 21:41:46 +0530

    updating ./tf-module-vpc

--- main.tf
+++ main.tf
@@ -6,3 +6,13 @@ resource "aws_vpc" "main" {
   )
 }
 
+resource "aws_subnet" "public_subnets" {
+  vpc_id = aws_vpc.main.id
+  tags = merge(
+    var.tags,
+    { Name = "${var.env}-vpc" }
+  )
+  for_each          = var.public_subnets
+  cidr_block        = each.value["cidr_block"]
+  availability_zone = each.value["availability_zone"]
+}
